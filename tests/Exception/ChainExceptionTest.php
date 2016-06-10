eb599f0e660124b2299c612976817a2e6ee446a7
Zoltán Borsos <zolli07@gmail.com>
2016-06-10 14:59:50 +0200

    *Created unit tests for ChainException BC-17

---
+++ tests/Exception/ChainExceptionTest.php
@@ -0,0 +1,25 @@
+<?php namespace BuildR\Foundation\Tests\Exception;
+
+class ChainExceptionTest extends \PHPUnit_Framework_TestCase {
+
+    public function testItCountsExceptionProperly() {
+        $this->markTestIncomplete();
+    }
+
+    public function testItCastToArrayProperly() {
+        $this->markTestIncomplete();
+    }
+
+    public function testItStoreOnlySubclassesOfException() {
+        $this->markTestIncomplete();
+    }
+
+    public function testItStoreExceptionsCorrectly() {
+        $this->markTestIncomplete();
+    }
+
+    public function testItCanReturnValidIterator() {
+        $this->markTestIncomplete();
+    }
+
+}
